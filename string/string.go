ac98ec7ba6d1e5b3341e80d9d4fbf8fbea77c46c
wilmansirit <wsirit@gmail.com>
2016-06-26 16:53:20 -0400

    Desde el Repositorio configurado para ATOM

--- string/string.go
+++ string/string.go
@@ -1,10 +1,19 @@
 package string
 
+// Reverse function: returns its argument string reversed rune-wise left to right.
 func Reverse(s string) string {
-    b := []rune(s)
-    for i := 0; i < len(b)/2; i++ {
-        j := len(b) - i - 1
-        b[i], b[j] = b[j], b[i]
-    }
-    return string(b)
+	b := []rune(s)
+	for i := 0; i < len(b)/2; i++ {
+		j := len(b) - i - 1
+		b[i], b[j] = b[j], b[i]
+	}
+	return string(b)
 }
+
+// func Reverse(s string) string {
+// 	r := []rune(s)
+// 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+// 		r[i], r[j] = r[j], r[i]
+// 	}
+// 	return string(r)
+// }
